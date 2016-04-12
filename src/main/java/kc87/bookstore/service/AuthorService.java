09ee6c38d298f061165fad2713c763c17d63b37e
citizen4 <citizen4@users.noreply.github.com>
2016-04-12 08:48:06 +0200

    JavaDoc added

--- src/main/java/kc87/bookstore/service/AuthorService.java
+++ src/main/java/kc87/bookstore/service/AuthorService.java
@@ -5,10 +5,22 @@ import kc87.bookstore.domain.Author;
 import java.util.List;
 
 /**
- * Service interface defining all Author service business actions
+ * Service interface defining all Author service business actions.
  */
 public interface AuthorService {
+   /**
+    * Returns a list of all known authors.
+    * 
+    * @return list of author entities
+    */
    List<Author> findAll();
 
+   /**
+    *  Find an author by name.
+    * 
+    * @param firstName the first name of the author
+    * @param lastName the last name of the author
+    * @return found author entity or null if non was found 
+    */
    Author findByName(final String firstName, final String lastName);
 }
